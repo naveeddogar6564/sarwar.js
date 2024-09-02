3abeca8bab965ffcf41e2c6ec1b62e374656334c
naveeddogar <naveeddewandogar@gmail.com>
2024-09-02 05:22:57 -0700

    update.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,10 @@ import React from "react"
 import image from "next/image"
 const page = () => {
   return ( 
-   <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md dark:bg-slate-800">
-   <img className="w-24 h-24 rounded-full mx-auto" src="/0 (14).jpg" alt="" width="384" height="512"/>
-   <div className="pt-6 text-center space-y-4">
-     <blockquote>
-       <p className="text-lg font-medium">
-       My name is Naveed sarwar dogar.i am working in govt sector.
-       i am very intrested in artifical inteligence.
-       thats why i joind Ai course under the supervision of 'Sir' sabzal shaheen and our amazing teacher 'Mr' mian Asif langrah.
-       both persons are so helpful and kind.
-       We learn lot of things from both persons.
-        
-       </p>
-     </blockquote>
-     <figcaption className="font-medium">
-       <div className="text-sky-500 dark:text-sky-400">
-        Naveed sarwar dogar
+   <div>
+    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/fone.png" alt="" width="384" height="512"/>
        </div>
-       <div className="text-slate-700 dark:text-slate-500">
-         Student Generative artificial inteligence course
-       </div>
-     </figcaption>
-   </div>
- </figure></div>
+    
     )
 }
 
